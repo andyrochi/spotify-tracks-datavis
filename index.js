fad277400baa2a30369e5841c8fb8e71d8990fde
Andy Chang <andy.rochi@gmail.com>
2022-11-21 15:38:07 +0800

    refactor: modularize bar chart

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const {
     csv
 } = d3;
 import { radarPlot } from './radarPlot.js';
+import { barChart } from './barChart.js';
 
 let data = {};
 let chosenAttribute = 'acousticness';
@@ -99,11 +100,6 @@ d3.select(this)
     .style("opacity", 0.8)
 }
 
-// set the dimensions and margins of the graph
-const marginBar = {top: 20, right: 30, bottom: 40, left: 90},
-    widthBar = 460 - marginBar.left - marginBar.right,
-    heightBar = 400 - marginBar.top - marginBar.bottom;
-
 const svgBar = d3
                 .select("#bar-chart");
 svgBar.append("g");
@@ -187,81 +183,15 @@ const render = () => {
 
     radarPlot(svgRadar, ticks, svgRadarDim, radarLabels, radarData);
     
-    const computePitchCount = (data) => {
-        // bar chart data
-        const dataBar = new Array(key_signature_map.length * mode_map.length).fill().map(() => ({value: 0, pitch: ""}));;
-        for (let i = 0; i < key_signature_map.length; i++) {
-            for (let j = 0; j < mode_map.length; j++) {
-                dataBar[i * mode_map.length + j]['pitch'] = `${key_signature_map[i]} ${mode_map[j]}`;
-            }
-        }
-        data.forEach((d,i) => {
-            const mode = d['mode'];
-            const key = d['key'];
-            dataBar[key * mode_map.length + mode]['value'] += 1;
-        })
-        return dataBar;
-    }
+    // set the dimensions and margins of the graph
+    const marginBar = {top: 20, right: 30, bottom: 40, left: 90},
+        widthBarSvg = 460,
+        heightBarSvg = 400;
     
-    const dataBar = computePitchCount(data);
-
-    // sort ascend
-    dataBar.sort((a, b) => {
-        return b['value'] - a['value'];
-    })
-    // // bar chart
-    svgBar
-        .attr("width", widthBar + marginBar.left + marginBar.right)
-        .attr("height", heightBar + marginBar.top + marginBar.bottom);
-    const svgBarG = svgBar.select("g")
-        .attr("transform", `translate(${marginBar.left}, ${marginBar.top})`);
-    
-        // Add X axis
-    const xBar = d3.scaleLinear()
-        .domain([0, d3.max(dataBar, (d) => d.value)])
-        .range([ 0, widthBar]);
-    const xBarAxis = svgBarG.selectAll("g.x-bar-axis").data([null]);
-    const xBarEnter = xBarAxis
-        .enter()
-        .append("g")
-            .attr("class", "x-bar-axis");
-        
-        xBarEnter.merge(xBarAxis)
-            .attr("transform", `translate(0, ${heightBar})`)
-            .call(d3.axisBottom(xBar))
-        .selectAll("text")
-            .attr("transform", "translate(-10,0)rotate(-45)")
-            .style("text-anchor", "end");
-    
-    // Y axis
-    const yBar = d3.scaleBand()
-    .range([ 0, heightBar ])
-    .domain(dataBar.map(d => d.pitch))
-    .padding(.1);
-
-    const yBarAxis = svgBarG
-        .selectAll("g.y-bar-axis").data([null]);
-    
-    const yBarAxisEnter = yBarAxis
-        .enter()
-            .append("g")
-            .attr("class", "y-bar-axis");
-    
-    yBarAxisEnter.merge(yBarAxis)
-        .call(d3.axisLeft(yBar));
-
-    //Bars
-    svgBarG.selectAll("myRect")
-    .data(dataBar)
-    .join("rect")
-        .attr("class", "myRect")
-        .attr("x", xBar(0) )
-        .attr("y", d => yBar(d.pitch))
-        .attr("width", d => xBar(d.value))
-        .attr("height", yBar.bandwidth())
-        .attr("fill", "#69b3a2");
+    barChart(svgBar, marginBar, heightBarSvg, widthBarSvg, key_signature_map, mode_map, data);
     
 };
+
 
 function debounce(func, timeout = 30) {
     let timer;
