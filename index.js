e52777268aad5e1068857a24b0bdaa5f369fecd4
Andy Chang <andy.rochi@gmail.com>
2022-11-21 14:35:28 +0800

    feat: Implement bar chart

--- index.js
+++ index.js
@@ -187,18 +187,29 @@ const render = () => {
 
     radarPlot(svgRadar, ticks, svgRadarDim, radarLabels, radarData);
     
-    // bar chart data
-    const dataBar = new Array(key_signature_map.length * mode_map.length).fill({value: 0, pitch: ""});
-    console.log(dataBar);
-    for (let i = 0; i < key_signature_map.length; i++) {
-        for (let j = 0; j < mode_map.length; j++) {
-            dataBar[i * mode_map.length + j]['pitch'] = `${key_signature_map[i]} ${mode_map[j]}`;
-            console.log(`${key_signature_map[i]} ${mode_map[j]}`);
+    const computePitchCount = (data) => {
+        // bar chart data
+        const dataBar = new Array(key_signature_map.length * mode_map.length).fill().map(() => ({value: 0, pitch: ""}));;
+        for (let i = 0; i < key_signature_map.length; i++) {
+            for (let j = 0; j < mode_map.length; j++) {
+                dataBar[i * mode_map.length + j]['pitch'] = `${key_signature_map[i]} ${mode_map[j]}`;
+            }
         }
+        data.forEach((d,i) => {
+            const mode = d['mode'];
+            const key = d['key'];
+            dataBar[key * mode_map.length + mode]['value'] += 1;
+        })
+        return dataBar;
     }
     
+    const dataBar = computePitchCount(data);
 
-    // bar chart
+    // sort ascend
+    dataBar.sort((a, b) => {
+        return b['value'] - a['value'];
+    })
+    // // bar chart
     svgBar
         .attr("width", widthBar + marginBar.left + marginBar.right)
         .attr("height", heightBar + marginBar.top + marginBar.bottom);
@@ -207,11 +218,17 @@ const render = () => {
     
         // Add X axis
     const xBar = d3.scaleLinear()
-        .domain([0, 13000])
+        .domain([0, d3.max(dataBar, (d) => d.value)])
         .range([ 0, widthBar]);
-        svgBarG.append("g")
-        .attr("transform", `translate(0, ${heightBar})`)
-        .call(d3.axisBottom(xBar))
+    const xBarAxis = svgBarG.selectAll("g.x-bar-axis").data([null]);
+    const xBarEnter = xBarAxis
+        .enter()
+        .append("g")
+            .attr("class", "x-bar-axis");
+        
+        xBarEnter.merge(xBarAxis)
+            .attr("transform", `translate(0, ${heightBar})`)
+            .call(d3.axisBottom(xBar))
         .selectAll("text")
             .attr("transform", "translate(-10,0)rotate(-45)")
             .style("text-anchor", "end");
@@ -219,20 +236,30 @@ const render = () => {
     // Y axis
     const yBar = d3.scaleBand()
     .range([ 0, heightBar ])
-    .domain(data.map(d => `${key_signature_map[d.key]} ${mode_map[d.mode]}`))
+    .domain(dataBar.map(d => d.pitch))
     .padding(.1);
-    svgBarG.append("g")
-    .call(d3.axisLeft(yBar));
+
+    const yBarAxis = svgBarG
+        .selectAll("g.y-bar-axis").data([null]);
+    
+    const yBarAxisEnter = yBarAxis
+        .enter()
+            .append("g")
+            .attr("class", "y-bar-axis");
+    
+    yBarAxisEnter.merge(yBarAxis)
+        .call(d3.axisLeft(yBar));
 
     //Bars
     svgBarG.selectAll("myRect")
-    .data(data)
+    .data(dataBar)
     .join("rect")
-    .attr("x", xBar(0) )
-    .attr("y", d => yBar(`${key_signature_map[d.key]} ${mode_map[d.mode]}`))
-    .attr("width", d => xBar(d.Value))
-    .attr("height", yBar.bandwidth())
-    .attr("fill", "#69b3a2");
+        .attr("class", "myRect")
+        .attr("x", xBar(0) )
+        .attr("y", d => yBar(d.pitch))
+        .attr("width", d => xBar(d.value))
+        .attr("height", yBar.bandwidth())
+        .attr("fill", "#69b3a2");
     
 };
 
