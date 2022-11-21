4a5a832c2aa892d4f992ae1c03793e5eddbfd116
Andy Chang <andy.rochi@gmail.com>
2022-11-21 18:05:07 +0800

    feat: Add multiple histograms in one chart and bar sorting

--- index.js
+++ index.js
@@ -39,12 +39,15 @@ const xMax = {
 
 const key_signature_map = ["C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B"];
 const mode_map = ["minor", "major"];
+const selectedGenre = ['j-rock', 'j-pop'];
 
 // set the dimensions and margins of the graph
 const margin = {top: 10, right: 30, bottom: 30, left: 50},
 width = 460 - margin.left - margin.right,
 height = 400 - margin.top - margin.bottom;
 let nBin = 10;
+
+const defaultOpacity = 0.5;
 
 const svg = select('svg#plot')
     .attr("width", width + margin.left + margin.right)
@@ -97,12 +100,14 @@ TooltipHistogram
     .style("opacity", 0)
 d3.select(this)
     .style("stroke", "none")
-    .style("opacity", 0.8)
+    .style("opacity", defaultOpacity)
 }
 
 const svgBar = d3
                 .select("#bar-chart");
 svgBar.append("g");
+
+const accent = d3.scaleOrdinal(d3.schemeAccent);
 
 const render = () => {
     // X axis: scale and draw:
@@ -132,36 +137,50 @@ const render = () => {
     
     
     // And apply this function to data to get the bins
-    const bins = histogram(data);
+    let bins = selectedGenre.map(genre => histogram(data.filter((d) => d['track_genre'] === genre)));
+    if (bins.length === 0)
+        bins = [histogram(data)];
 
-    // console.log('bins:', bins);
+    const binMax = bins.map((bin) => (d3.max(bin, (d) => d.length)));
 
-    // Y axis: update now that we know the domain
-    y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
+    // Y axis: update now that we know the domain, remember to desconstruct
+    y.domain([0, Math.max(...binMax)]);   // d3.hist has to be called before the Y axis obviously
     yAxis
         .transition()
         .duration(1000)
         .call(d3.axisLeft(y));
     
-    // Join the rect with the bins data
-    const u = svg.selectAll("rect")
-        .data(bins);
+    bins.forEach((bin, i) => {
+        const cssSelector = `.rect-${i}`;
 
-    // Manage the existing bars and eventually the new ones:
-    const uJoin = u
-        .join("rect");
-    uJoin // Add a new rect for each new elements
-        .transition() // and apply changes to all of them
-        .duration(1000)
-            .attr("x", 1)
-            .attr("transform", function(d) { return `translate(${x(d.x0)}, ${y(d.length)})`})
-            .attr("width", function(d) { return x(d.x1) - x(d.x0) - 1; })
-            .attr("height", function(d) { return height - y(d.length); })
-            .style("fill", "#69b3a2");
-    uJoin
-        .on("mouseover", mouseoverHistogram)
-        .on("mousemove", mousemoveHistogram)
-        .on("mouseleave", mouseleaveHistogram);
+        const u = svg.selectAll(cssSelector)
+            .data(bin);
+
+        // Manage the existing bars and eventually the new ones:
+        const uJoin = u
+            .join("rect");
+
+        console.log('uJoin', uJoin);
+
+        uJoin // Add a new rect for each new elements
+            // .attr("class", `rect-${i}`)
+            .transition() // and apply changes to all of them
+            .duration(1000)
+                .attr("x", 1)
+                .attr("class", function(d, j) { return `rect-bin-${j}` + " " + `rect-${i}`} )
+                .attr("transform", function(d) { return `translate(${x(d.x0)}, ${y(d.length)})`})
+                .attr("width", function(d) { return x(d.x1) - x(d.x0) - 1; })
+                .attr("height", function(d) { return height - y(d.length); })
+                .attr("opacity", defaultOpacity)
+                .style("fill", () => {return accent(i)});
+        uJoin
+            .on("mouseover", mouseoverHistogram)
+            .on("mousemove", mousemoveHistogram)
+            .on("mouseleave", mouseleaveHistogram);
+    })
+    
+    const allRects = svg.selectAll("rect");
+    allRects.sort((a, b) =>  b.length - a.length );
     
     // radial chart
     const ticks = [0.2, 0.4, 0.6, 0.8];
